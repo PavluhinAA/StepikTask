f158daebe55cb3af7272260e1cfa9d70b33544e5
Антон <pavlukhin.ant0n@yandex.ru>
2024-11-11 19:14:22 +0300

    30.10.24 interface

--- StepikTask/main.go
+++ StepikTask/main.go
@@ -2,25 +2,30 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	"strings"
 )
 
-func main() {
-	fn := func(input uint) uint {
-		var res string
-		for i := 0; i < len(strconv.Itoa(int(input))); i++ {
-			if int(strconv.Itoa(int(input))[i])%2 == 0 {
-				if string(strconv.Itoa(int(input))[i]) != "0" {
-					res += string(strconv.Itoa(int(input))[i])
-				}
-			}
-		}
-		if res == "" {
-			return 100
-		}
-		res2, _ := strconv.ParseInt(res, 10, 64)
-		x := uint(res2)
-		return x
+type Stringer interface {
+	String() string
+}
+
+type battery struct {
+	zeus string
+}
+
+func (b *battery) String() string {
+	var res string
+	for i := 0; i < strings.Count(b.zeus, "0"); i++ {
+		res += "\t"
 	}
-	fmt.Println(fn(12657650001313013))
+	for i := 0; i < strings.Count(b.zeus, "1"); i++ {
+		res += "X"
+	}
+	return res
+}
+func main() {
+	var zeus string
+	fmt.Scan(&zeus)
+	batteryForTest := battery{zeus: zeus}
+
 }
