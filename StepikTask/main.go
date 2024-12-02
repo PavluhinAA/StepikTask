52a63965973ea5e3f69514e336dc44dda6d63e14
Антон <pavlukhin.ant0n@yandex.ru>
2024-12-02 20:40:33 +0300

    Hello Git

--- StepikTask/main.go
+++ StepikTask/main.go
@@ -1,31 +1,8 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
 
-type Stringer interface {
-	String() string
-}
-
-type battery struct {
-	zeus string
-}
-
-func (b *battery) String() string {
-	var res string
-	for i := 0; i < strings.Count(b.zeus, "0"); i++ {
-		res += "\t"
-	}
-	for i := 0; i < strings.Count(b.zeus, "1"); i++ {
-		res += "X"
-	}
-	return res
-}
 func main() {
-	var zeus string
-	fmt.Scan(&zeus)
-	batteryForTest := battery{zeus: zeus}
-
+	var input string
+	fmt.Scan(&input)
 }
